2a2850d28e90eb10ff158e7a0c205aba238e4c3b
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2022-10-05 23:54:26 +0530

    Made the top area of HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,9 @@
 import React, { useLayoutEffect } from 'react'
-import { Image, Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Image, ScrollView, Text, TextInput, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation } from '@react-navigation/native'
+import { UserIcon, ChevronDownIcon, MagnifyingGlassIcon, AdjustmentsVerticalIcon } from 'react-native-heroicons/outline'
+import Categories from '../components/Categories'
 
 const HomeScreen = () => {
   const navigation = useNavigation()
@@ -13,7 +15,7 @@ const HomeScreen = () => {
   }, [])
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5">
       <View className='flex-row pb-3 items-center mx-4 space-x-2'>
         <Image
           source={{
@@ -25,11 +27,30 @@ const HomeScreen = () => {
           }}
           className='h-7 w-7 bg-gray-300 p-4 rounded-full'
         />
-        <View>
+        <View className="flex-1">
           <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
-          <Text className='font-bold text-xl'>Current Location</Text>
+          <Text className='font-bold text-xl'>
+            Current Location
+            <ChevronDownIcon size={20} color="#00ccbb" />
+          </Text>
         </View>
+        <UserIcon size={35} color="#00ccbb" />
       </View>
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon size={20} color="gray" />
+          <TextInput placeholder='Restaurants and cuisines' keyboardType='default' />
+        </View>
+        <AdjustmentsVerticalIcon color="#00ccbb" />
+      </View>
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{
+          paddingBottom: 100
+        }}
+      >
+        <Categories />
+      </ScrollView>
     </SafeAreaView>
   )
 }
